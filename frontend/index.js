4e68d27b872119f41e976b6835e4e885eba4b1b4
VitaBer <vita.berziunaite@gmail.com>
2021-02-24 21:14:32 +0200

    Moved frontend into separate folder

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3003/article/amount=100")
+fetch("http://localhost:3000/article/amount=100")
   .then((response) => response.json())
   .then((data) => showArticles(data));
 
